79084605764be3f18c1b454a23ad9eb45437f78a
GRK <gitkey.aluminum361@passmail.net>
2025-08-22 23:12:08 +0200

    chore: remove old proptypes pattern

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import PropTypes from 'prop-types';
 
 const Button = ({ text, linkStyles, buttonStyles, link = "#" }: { text: string, linkStyles: string, buttonStyles: string, link?: string }) => (
   <Link href={link} className={linkStyles}>
@@ -8,12 +7,5 @@ const Button = ({ text, linkStyles, buttonStyles, link = "#" }: { text: string, 
     </button>
   </Link>
 );
-
-Button.propTypes = {
-  text: PropTypes.string.isRequired,
-  linkStyles: PropTypes.string.isRequired,
-  buttonStyles: PropTypes.string.isRequired,
-  link: PropTypes.string,
-};
 
 export default Button;
