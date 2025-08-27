c95e45655c67175b21511c08e93027f5155c9a3e
GRK <gitkey.aluminum361@passmail.net>
2025-08-27 19:42:49 +0200

    refactor: update app metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,18 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "ZKS",
   description: "Generate and verify zk proofs of solvency",
-  // viewport: viewPort,
+  icons: {
+    icon: [
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon.ico', sizes: 'any' },
+    ],
+    shortcut: "/favicon.ico",
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
+  },
+  manifest: "/site.webmanifest",
 };
 
 export default function RootLayout(props: { children: ReactNode }) {
